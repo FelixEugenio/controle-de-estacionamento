497c7beb76ee66a58dd2646ac23abbca140a167c
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-03-26 21:44:52 +0000

    feat: add save method to ParkingSpotServices

--- src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -2,7 +2,10 @@ package com.api.parking_control.services;
 
 import org.springframework.stereotype.Service;
 
+import com.api.parking_control.models.ParkingSpot;
 import com.api.parking_control.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -13,5 +16,9 @@ public class ParkingSpotService {
     this.parkingSpotRepository = parkingSpotRepository;
   }
 
-  
+  @Transactional
+  public ParkingSpot save(ParkingSpot parkingSpot) {
+    return parkingSpotRepository.save(parkingSpot);
+  }
+
 }
