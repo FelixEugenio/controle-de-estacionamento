665bb159e78324e226f27d55ca33d60098e7af99
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-03-26 21:17:42 +0000

    feat: add ParkingSpotService

---
+++ src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -0,0 +1,12 @@
+package com.api.parking_control.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+    
+}
--- src/main/java/com/api/parking_control/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parking_control/repositories/ParkingSpotRepository.java
@@ -3,9 +3,11 @@ package com.api.parking_control.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.api.parking_control.models.ParkingSpot;
 
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID> {
     
 }
