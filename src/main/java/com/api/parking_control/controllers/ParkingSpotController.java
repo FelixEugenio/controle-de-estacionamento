5710e0d787e5327e60e75dd31fb294c75bd531d1
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-03-26 21:42:29 +0000

    feat: add ParkingSpotController

--- src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -1,10 +1,22 @@
 package com.api.parking_control.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.parking_control.dtos.ParkingSpotDtos;
+import com.api.parking_control.models.ParkingSpot;
 import com.api.parking_control.services.ParkingSpotService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -15,6 +27,14 @@ public class ParkingSpotController {
 
     public ParkingSpotController(ParkingSpotService parkingSpotService) {
         this.parkingSpotService = parkingSpotService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ParkingSpot> saveParkingSpot(@RequestBody @Valid ParkingSpotDtos parkingSpotDtos) {
+        var parkingSpot = new ParkingSpot();
+        BeanUtils.copyProperties(parkingSpotDtos, parkingSpot);
+        parkingSpot.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpot));
     }
 
     
